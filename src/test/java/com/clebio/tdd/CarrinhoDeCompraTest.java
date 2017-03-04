28b887acf1cc2e7412787fa260580735561839d8
Clebio <clebiovieira@gmail.com>
2017-03-04 18:45:50 -0300

    Cap6

--- src/test/java/com/clebio/tdd/CarrinhoDeCompraTest.java
+++ src/test/java/com/clebio/tdd/CarrinhoDeCompraTest.java
@@ -2,23 +2,31 @@ package com.clebio.tdd;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CarrinhoDeCompraTest {
+	
+	private CarrinhoDeCompras carrinho;
+	
+	@Before
+	public void inicializa() {
+		this.carrinho = new CarrinhoDeCompras();
+	}
+
 	@Test
-	public void deveRetornarZeroSeCarrinhoVazio() {
-		CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+	public void deveRetornarZeroSeCarrinhoVazio() {	
 		assertEquals(0.0, carrinho.maiorValor(), 0.0001);
 	}
 	@Test
 	public void deveRetornarValorDoItemSeCarrinhoCom1Elemento() {
-		CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
 		carrinho.adiciona(new Item("Geladeira", 1, 900.0));
+		
 		assertEquals(900.0, carrinho.maiorValor(), 0.0001);
 	}
 	@Test
 	public void deveRetornarMaiorValorSeCarrinhoContemMuitosElementos() {
-		CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+		
 		carrinho.adiciona(new Item("Geladeira", 1, 900.0));
 		carrinho.adiciona(new Item("Fogão", 1, 1500.0));
 		carrinho.adiciona(new Item("Máquina de Lavar", 1, 750.0));
---
+++ src/test/java/com/clebio/tdd/CarrinhoDeComprasBuilder.java
@@ -0,0 +1,20 @@
+package com.clebio.tdd;
+
+public class CarrinhoDeComprasBuilder {
+	public CarrinhoDeCompras carrinho;
+	
+	public CarrinhoDeComprasBuilder() {
+		this.carrinho = new CarrinhoDeCompras();
+	}
+	
+	public CarrinhoDeComprasBuilder comItens(double... valores) {
+		for(double valor : valores) {
+			carrinho.adiciona(new Item("item", 1, valor));
+		}
+		return this;
+	}
+	
+	public CarrinhoDeCompras cria() {
+		return carrinho;
+	}
+}
